84cc9e17409a698f0b7dbe75aeaab6772d1d788d
Mustafa Tugrul Avci <mustafa.tugrulavci@alcumus.com>
2021-05-02 21:38:34 -0400

    window.prompt and window.confirm is set

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-var passwordGenerate = function(){
-    var passwordLenght = window.prompt("Please state length of the password- it should be greater than 5 and lower than 13");
-    var userNumEntry = Number(passwordLenght);
 
-    if (  userNumEntry < 5)  {
+
+var passwordGenerate = function(){
+    var passwordLenght = Number(window.prompt("Please state length of the password- it should be greater than 7 and lower than 129") );
+
+
+    if (passwordLenght < 8 || passwordLenght>128|| passwordLenght=== '' || isNaN(passwordLenght) ) {
         window.alert("please enter a valid number");
         passwordGenerate();
-
     }
+    var passwordLowercase = Number(window.confirm("Do you want lowercase in the password?") );
+    var passwordUppercase = Number(window.confirm("Do you want uppercase in the password?") );
+    var passwordNumbers = Number(window.confirm("Do you want numbers in the password?") );
+    var passwordSpeacialCharacter = Number(window.confirm("Do you want special characters in the password?") );
+
+
+
 
 };
 passwordGenerate();
