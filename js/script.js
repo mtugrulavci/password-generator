9a618892925be99b90da20c61fe7c30cf86e78f9
Mustafa Tugrul Avci <mustafa.tugrulavci@alcumus.com>
2021-05-03 20:54:24 -0400

    bug fixed and password added into html and windows alert

--- js/script.js
+++ js/script.js
@@ -5,14 +5,16 @@ const symbols = "!@#$%^&*_-+=";
 var characters = "";
 var password = "";
 
+
 var askCriteria = function() {
     var passwordLowercase = window.confirm("Do you want lowercase in the password?" );
     var passwordUppercase = window.confirm("Do you want uppercase in the password?") ;
     var passwordNumbers = window.confirm("Do you want numbers in the password?") ;
     var passwordSpeacialCharacter = window.confirm("Do you want special characters in the password?") ;
     if(passwordLowercase === false && passwordUppercase===false &&passwordNumbers===false&&passwordSpeacialCharacter===false){
-        return  window.alert("you have to select one condition"), askCriteria();
+      return  window.alert("you have to select one condition"),askCriteria();
     }
+    characters = "";
     if (passwordLowercase){
         characters = characters + alphaLower;
     }
@@ -22,25 +24,30 @@ var askCriteria = function() {
     if (passwordNumbers){
         characters = characters + numbers;
     }
-    if (symbols){
+    if (passwordSpeacialCharacter){
         characters = characters +symbols;
     }
 
 };
 
 var passwordGenerate = function(){
+    password = "";
     var passwordLenght = Number(window.prompt("Please state length of the password- it should be greater than 7 and lower than 129") );
     if (passwordLenght < 8 || passwordLenght>128|| passwordLenght=== '' || isNaN(passwordLenght) ) {
-        window.alert("please enter a valid number");
-        askLenght();
-    }
-    askCriteria();
+      return  window.alert("please enter a valid number"), passwordGenerate();
+    }else {
+        askCriteria();
+        
+        
     for (var i = 0; i < passwordLenght; i++){
         password += characters.charAt(
             Math.floor(Math.random() * characters.length)
           );
         }
-        return window.console.log (password);  
+    }
+    return  document.getElementById("passwordText").innerHTML = password, window.alert(password); ;
+
 };
 
-passwordGenerate();
+document.getElementById("submit").addEventListener("click", passwordGenerate);
+
