2782b8d571569172cb63c986f5a1a4bbc24559f2
Mustafa Tugrul Avci <mustafa.tugrulavci@alcumus.com>
2021-05-04 23:48:00 -0400

    starter code applied,js functions changed accordingly

--- js/script.js
+++ js/script.js
@@ -1,53 +1,79 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Assignment code here
 const alphaLower = "abcdefghijklmnopqrstuvwxyz";
 const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 var characters = "";
 var password = "";
-
+var passwordLenght = 0;
+var container ="";
 
 var askCriteria = function() {
-    var passwordLowercase = window.confirm("Do you want lowercase in the password?" );
-    var passwordUppercase = window.confirm("Do you want uppercase in the password?") ;
-    var passwordNumbers = window.confirm("Do you want numbers in the password?") ;
-    var passwordSpeacialCharacter = window.confirm("Do you want special characters in the password?") ;
-    if(passwordLowercase === false && passwordUppercase===false &&passwordNumbers===false&&passwordSpeacialCharacter===false){
-      return  window.alert("you have to select one condition"),askCriteria();
-    }
+  var passwordLowercase = window.confirm("Do you want lowercase in the password?" );
+  var passwordUppercase = window.confirm("Do you want uppercase in the password?") ;
+  var passwordNumbers = window.confirm("Do you want numbers in the password?") ;
+  var passwordSpeacialCharacter = window.confirm("Do you want special characters in the password?") ;
+  if(passwordLowercase === false && passwordUppercase===false &&passwordNumbers===false&&passwordSpeacialCharacter===false){
+    return  window.alert("you have to select one condition"),askCriteria();
+  }
     characters = "";
+    container = "";
     if (passwordLowercase){
-        characters = characters + alphaLower;
+        container = container + alphaLower;
     }
     if (passwordUppercase){
-        characters = characters + alphaUpper;
+      container = container + alphaUpper;
     }
     if (passwordNumbers){
-        characters = characters + numbers;
+      container = container + numbers;
     }
     if (passwordSpeacialCharacter){
-        characters = characters +symbols;
+      container = container +symbols;
     }
 
-};
+for (var k = 0; k < passwordLenght ; k++){
+  if (passwordLowercase && k === 0) {
+    characters+=  alphaLower.charAt(
+      Math.floor(Math.random() * alphaLower.length));
+      }
 
-var passwordGenerate = function(){
-    password = "";
-    var passwordLenght = Number(window.prompt("Please state length of the password- it should be greater than 7 and lower than 129") );
-    if (passwordLenght < 8 || passwordLenght>128|| passwordLenght=== '' || isNaN(passwordLenght) ) {
-      return  window.alert("please enter a valid number"), passwordGenerate();
-    }else {
-        askCriteria();
-        
-        
-    for (var i = 0; i < passwordLenght; i++){
-        password += characters.charAt(
-            Math.floor(Math.random() * characters.length)
-          );
+  else if (passwordUppercase&& k===1){
+        characters+=alphaUpper.charAt(
+          Math.floor(Math.random() * alphaUpper.length))
         }
-    }
-    return  document.getElementById("passwordText").innerHTML = password, window.alert(password); ;
+  else if (passwordNumbers&& k===2){
+        characters+=numbers.charAt(
+          Math.floor(Math.random() * numbers.length))
+        }
+  else if (passwordSpeacialCharacter&& k===3){
+        characters+=symbols.charAt(
+          Math.floor(Math.random() * symbols.length))
+          }
+  else {
+    characters += container.charAt(
+      Math.floor(Math.random() * container.length))
+       }
+  }
+  password=characters;
+}
+  
 
-};
+// Write password to the #password input
+function writePassword() {
+  password = "";
+  passwordLenght = Number(window.prompt("Please state length of the password- it should be greater than 7 and lower than 129") );
+  if (passwordLenght < 8 || passwordLenght>128|| passwordLenght=== '' || isNaN(passwordLenght) ) {
+    return  window.alert("please enter a valid number"), writePassword();
+  }else {
+    askCriteria();
 
-document.getElementById("submit").addEventListener("click", passwordGenerate);
+  } 
+        var passwordText = document.querySelector("#password")
+        return passwordText.value = password,window.alert(password); 
+}
+// Add event listener to generate button
+      generateBtn.addEventListener("click", writePassword);
 
